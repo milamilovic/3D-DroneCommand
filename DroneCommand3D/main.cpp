f1625ddcdca5857e0240877034291d90b8e5a680
unknown <milamilovic03@gmail.com>
2025-01-09 18:51:51 +0100

    init main

--- DroneCommand3D/main.cpp
+++ DroneCommand3D/main.cpp
@@ -1,20 +1,214 @@
-// DroneCommand3D.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Autor: Mila Milovic
+
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main()
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+unsigned int compileShader(GLenum type, const char* source);
+unsigned int createShader(const char* vsSource, const char* fsSource);
+static unsigned loadImageToTexture(const char* filePath);
+
+int main(void)
 {
-    std::cout << "Hello World!\n";
+    if (!glfwInit())
+    {
+        std::cout << "GLFW Biblioteka se nije ucitala! :(\n";
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window;
+    unsigned int wWidth = 1044;
+    unsigned int wHeight = 800;
+    const char wTitle[] = "Drone command 3D";
+    window = glfwCreateWindow(wWidth, wHeight, wTitle, NULL, NULL);
+
+    if (window == NULL)
+    {
+        std::cout << "Prozor nije napravljen! :(\n";
+        glfwTerminate();
+        return 2;
+    }
+
+    glfwMakeContextCurrent(window);
+
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "GLEW nije mogao da se ucita! :'(\n";
+        return 3;
+    }
+
+    unsigned int basicShader = createShader("basic.vert", "basic.frag");
+
+    float vertices[] =
+    {
+        0.25, 0.0,    1.0, 0.0, 0.0, 1.0,
+       -0.25, 0.0,    0.0, 0.0, 1.0, 1.0,
+        0.0 ,-0.5,    1.0, 1.0, 1.0, 1.0
+    };
+    unsigned int stride = (2 + 4) * sizeof(float);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, stride, (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, stride, (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+    glClearColor(0.15, 0.15, 0.15, 1.0);
+
+    while (!glfwWindowShouldClose(window)) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(window, GL_TRUE);
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(basicShader);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        glUseProgram(0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(basicShader);
+    glfwTerminate();
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+unsigned int compileShader(GLenum type, const char* source)
+{
+    std::string content = "";
+    std::ifstream file(source);
+    std::stringstream ss;
+    if (file.is_open())
+    {
+        ss << file.rdbuf();
+        file.close();
+        std::cout << "Uspjesno procitao fajl sa putanje \"" << source << "\"!" << std::endl;
+    }
+    else {
+        ss << "";
+        std::cout << "Greska pri citanju fajla sa putanje \"" << source << "\"!" << std::endl;
+    }
+    std::string temp = ss.str();
+    const char* sourceCode = temp.c_str();
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    int shader = glCreateShader(type);
+
+    int success;
+    char infoLog[512];
+    glShaderSource(shader, 1, &sourceCode, NULL);
+    glCompileShader(shader);
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (success == GL_FALSE)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        if (type == GL_VERTEX_SHADER)
+            printf("VERTEX");
+        else if (type == GL_FRAGMENT_SHADER)
+            printf("FRAGMENT");
+        printf(" sejder ima gresku! Greska: \n");
+        printf(infoLog);
+    }
+    return shader;
+}
+unsigned int createShader(const char* vsSource, const char* fsSource)
+{
+
+    unsigned int program;
+    unsigned int vertexShader;
+    unsigned int fragmentShader;
+
+    program = glCreateProgram();
+
+    vertexShader = compileShader(GL_VERTEX_SHADER, vsSource);
+    fragmentShader = compileShader(GL_FRAGMENT_SHADER, fsSource);
+
+
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(program, GL_VALIDATE_STATUS, &success);
+    if (success == GL_FALSE)
+    {
+        glGetShaderInfoLog(program, 512, NULL, infoLog);
+        std::cout << "Objedinjeni sejder ima gresku! Greska: \n";
+        std::cout << infoLog << std::endl;
+    }
+
+    glDetachShader(program, vertexShader);
+    glDeleteShader(vertexShader);
+    glDetachShader(program, fragmentShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
+}
+static unsigned loadImageToTexture(const char* filePath) {
+    int TextureWidth;
+    int TextureHeight;
+    int TextureChannels;
+    unsigned char* ImageData = stbi_load(filePath, &TextureWidth, &TextureHeight, &TextureChannels, 0);
+    if (ImageData != NULL)
+    {
+        stbi__vertical_flip(ImageData, TextureWidth, TextureHeight, TextureChannels);
+
+        GLint InternalFormat = -1;
+        switch (TextureChannels) {
+        case 1: InternalFormat = GL_RED; break;
+        case 2: InternalFormat = GL_RG; break;
+        case 3: InternalFormat = GL_RGB; break;
+        case 4: InternalFormat = GL_RGBA; break;
+        default: InternalFormat = GL_RGB; break;
+        }
+
+        unsigned int Texture;
+        glGenTextures(1, &Texture);
+        glBindTexture(GL_TEXTURE_2D, Texture);
+        glTexImage2D(GL_TEXTURE_2D, 0, InternalFormat, TextureWidth, TextureHeight, 0, InternalFormat, GL_UNSIGNED_BYTE, ImageData);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        stbi_image_free(ImageData);
+        return Texture;
+    }
+    else
+    {
+        std::cout << "Textura nije ucitana! Putanja texture: " << filePath << std::endl;
+        stbi_image_free(ImageData);
+        return 0;
+    }
+}
